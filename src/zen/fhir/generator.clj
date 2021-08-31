4d10e5bed2b7a0dddc6e2f4652a8fbb45c87b768
KGOH <kgofhedgehogs@gmail.com>
2021-08-31 14:19:32 +0200

    Add new zen schema generator ns generation

--- src/zen/fhir/generator.clj
+++ src/zen/fhir/generator.clj
@@ -1,4 +1,34 @@
-(ns zen.fhir.generator)
+(ns zen.fhir.generator
+  (:require [zen.fhir.utils :as utils]))
+
+
+(defn generate-zen-schema [fhir-inter [url inter-res]]
+  (let [sd-inter    (get fhir-inter "StructureDefinition")
+        schema-ns   (:zen.fhir/schema-ns inter-res)
+        imports     (into #{}
+                          (keep (comp :zen.fhir/schema-ns sd-inter))
+                          (keys (apply concat (vals (:deps inter-res)))))
+        base-schema (some-> (get-in sd-inter [(:baseDefinition inter-res) :zen.fhir/schema-ns])
+                            name
+                            (symbol "schema"))]
+    {schema-ns {'ns     schema-ns
+                'import imports
+                'schema (utils/strip-nils
+                          {:zen/tags #{'zen/schema}
+                           :confirms (when base-schema #{base-schema})})}}))
+
+
+(defn generate-zen-schemas* [fhir-inter]
+  (into {}
+        (map (partial generate-zen-schema fhir-inter))
+        (get fhir-inter "StructureDefinition")))
+
+
+
+(defn generate-zen-schemas [ztx]
+  (swap! ztx assoc :fhir.zen/ns (generate-zen-schemas* (:fhir/inter @ztx)))
+  :done)
+
 
 
 ;; * resources, types
--- test/zen/fhir/generator_test.clj
+++ test/zen/fhir/generator_test.clj
@@ -1,6 +1,45 @@
 (ns zen.fhir.generator-test
   (:require
    [zen.fhir.generator :as sut]
+   [zen.fhir.core]
    [zen.core]
    [matcho.core :as matcho]
    [clojure.test :as t]))
+
+
+(t/deftest generate-project-integration
+
+  (def ztx  (zen.core/new-context {}))
+
+  (zen.fhir.core/load-all ztx "hl7.fhir.us.core"
+                          {:params {"hl7.fhir.r4.core" {:zen.fhir/package-ns 'fhir.r4}
+                                    "hl7.fhir.us.core" {:zen.fhir/package-ns 'us-core.v3}}})
+
+  (get-in @ztx [:fhir/inter "StructureDefinition" "http://hl7.org/fhir/StructureDefinition/patient-nationality"])
+
+  (sut/generate-zen-schemas ztx)
+
+  (matcho/match
+    (:fhir.zen/ns @ztx)
+    {'fhir.r4.Element
+     {'ns     'fhir.r4.Element
+      'schema {}}
+
+     'fhir.r4.Resource
+     {'ns     'fhir.r4.Resource
+      'schema {}}
+
+     'fhir.r4.DomainResource
+     {'ns     'fhir.r4.DomainResource
+      'import #(contains? % 'fhir.r4.Resource)
+      'schema {:confirms #(contains? % 'fhir.r4.Resource/schema)}}
+
+     'fhir.r4.Patient
+     {'ns     'fhir.r4.Patient
+      'import #(contains? % 'fhir.r4.DomainResource)
+      'schema {:confirms #(contains? % 'fhir.r4.DomainResource/schema)}}
+
+     'us-core.v3.us-core-patient
+     {'ns     'us-core.v3.us-core-patient
+      'import #(contains? % 'fhir.r4.Patient)
+      'schema {:confirms #(contains? % 'fhir.r4.Patient/schema)}}}))
