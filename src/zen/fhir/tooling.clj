6580aef761b2d645612a3f4e26a476f74a537676
KGOH <kgofhedgehogs@gmail.com>
2021-09-02 13:02:29 +0200

    Add tooling/-main package version arg
    Co-authored-by: @ddegr <de@health-samurai.io>

--- src/zen/fhir/tooling.clj
+++ src/zen/fhir/tooling.clj
@@ -6,12 +6,12 @@
             [clojure.java.io :as io]))
 
 
-(defn -main [node-modules-folder zrc-dir]
+(defn -main [node-modules-folder zrc-dir package-ver]
   (let [ztx (zen-core/new-context {})]
     (load-all ztx nil {:params {"hl7.fhir.r4.core" {:zen.fhir/package-ns 'fhir-r4}
                                 "hl7.fhir.us.core" {:zen.fhir/package-ns 'us-core-v3}
                                 "hl7.fhir.us.carin-bb" {:zen.fhir/package-ns 'carin-bb-v1}}
                        :node-modules-folder node-modules-folder})
     (generate-zen-schemas ztx)
-    (spit-zen-npm-modules ztx zrc-dir))
+    (spit-zen-npm-modules ztx zrc-dir package-ver))
   (prn :done))
