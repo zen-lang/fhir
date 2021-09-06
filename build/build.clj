baf9fef5a026bdbfa2b2f3dbbfd82ffa278af383
KGOH <kgofhedgehogs@gmail.com>
2021-09-06 14:32:33 +0200

    Rename build jar file
    Co-authored-by: @ghrpp <profunctorlense@gmail.com>

--- build/build.clj
+++ build/build.clj
@@ -5,7 +5,7 @@
 (def version (format "1.2.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
-(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+(def uber-file (format "target/zen-fhir-standalone.jar" (name lib) version))
 
 (defn clean [_]
   (b/delete {:path "target"}))
