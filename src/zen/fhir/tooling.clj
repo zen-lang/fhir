af908a899930464645fbfbae3e871aca6e3bbac3
KGOH <kgofhedgehogs@gmail.com>
2021-09-07 12:18:48 +0200

    Add fhir r3 & mcode package mcode name aliases
    Co-authored-by: @ghrpp <profunctorlense@gmail.com>

--- src/zen/fhir/tooling.clj
+++ src/zen/fhir/tooling.clj
@@ -9,8 +9,10 @@
 
 (defn -main [node-modules-folder zrc-dir package-ver & [package-name]]
   (let [ztx(zen-core/new-context {})]
-    (load-all ztx nil {:params {"hl7.fhir.r4.core" {:zen.fhir/package-ns 'fhir-r4}
-                                "hl7.fhir.us.core" {:zen.fhir/package-ns 'us-core-v3}
+    (load-all ztx nil {:params {"hl7.fhir.r4.core"     {:zen.fhir/package-ns 'fhir-r4}
+                                "hl7.fhir.r3.core"     {:zen.fhir/package-ns 'fhir-r3}
+                                "hl7.fhir.us.core"     {:zen.fhir/package-ns 'us-core-v3}
+                                "hl7.fhir.us.mcode"    {:zen.fhir/package-ns 'mcode-v1}
                                 "hl7.fhir.us.carin-bb" {:zen.fhir/package-ns 'carin-bb-v1}}
                        :node-modules-folder node-modules-folder})
     (generate-zen-schemas ztx)
