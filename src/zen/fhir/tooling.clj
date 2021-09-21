b33e22b5adc08e609a181009ed6be59460ca264b
ApricotLace <profunctorlense@gmail.com>
2021-09-21 17:28:13 +0300

    Aidbox project builder
    Co-authored-by: @yurii_makhotkin <kgofhedgehogs@gmail.com>

--- src/zen/fhir/tooling.clj
+++ src/zen/fhir/tooling.clj
@@ -7,13 +7,24 @@
             [clojure.java.io :as io]))
 
 
+(defn spit-aidbox-project [node-modules-folder zrc-dir package-ver & [project-name]]
+  (let [ztx (zen-core/new-context {})]
+    (load-all ztx nil {:node-modules-folder node-modules-folder})
+    (generate-zen-schemas ztx)
+    (spit-zen-npm-modules ztx (str zrc-dir "/node_modules") package-ver)
+    (spit (format "%s/%s-aidbox-project.edn" zrc-dir project-name)
+          (pr-str {'ns (str project-name "-aidbox-project")
+                   'import #{(symbol project-name)}}))
+    (prn :done)))
+
+
 (defn -main [node-modules-folder zrc-dir package-ver & [package-name]]
-  (let [ztx(zen-core/new-context {})]
+  (let [ztx (zen-core/new-context {})]
     (load-all ztx nil {#_#_:params {"hl7.fhir.r4.core"     {:zen.fhir/package-ns 'fhir-r4}
-                                "hl7.fhir.r3.core"     {:zen.fhir/package-ns 'fhir-r3}
-                                "hl7.fhir.us.core"     {:zen.fhir/package-ns 'us-core-v3}
-                                "hl7.fhir.us.mcode"    {:zen.fhir/package-ns 'mcode-v1}
-                                "hl7.fhir.us.carin-bb" {:zen.fhir/package-ns 'carin-bb-v1}}
+                                    "hl7.fhir.r3.core"     {:zen.fhir/package-ns 'fhir-r3}
+                                    "hl7.fhir.us.core"     {:zen.fhir/package-ns 'us-core-v3}
+                                    "hl7.fhir.us.mcode"    {:zen.fhir/package-ns 'mcode-v1}
+                                    "hl7.fhir.us.carin-bb" {:zen.fhir/package-ns 'carin-bb-v1}}
                        :node-modules-folder node-modules-folder})
     (generate-zen-schemas ztx)
     (spit-zen-npm-modules ztx zrc-dir package-ver package-name)
