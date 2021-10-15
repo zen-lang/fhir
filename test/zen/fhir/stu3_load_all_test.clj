d301ff376b80709e32823b184fa56363dc4982ff
ApricotLace <profunctorlense@gmail.com>
2021-10-15 19:56:06 +0300

    R3 generator workarounds

--- test/zen/fhir/stu3_load_all_test.clj
+++ test/zen/fhir/stu3_load_all_test.clj
@@ -1,9 +1,10 @@
 (ns zen.fhir.stu3-load-all-test
-  (:require [clojure.java.io :as io]
-            [clojure.test :as t]
+  (:require [clojure.test :as t]
+            [zen.fhir.generator :as gen]
             [matcho.core :as matcho]
             [zen.core]
-            [zen.fhir.core :as fhir]))
+            [zen.fhir.core :as fhir]
+            [clojure.java.io :as io]))
 
 (defonce ztx (zen.core/new-context {}))
 
@@ -23,8 +24,28 @@
      "http://terminology.hl7.org/CodeSystem/v3-ActMood"
      "http://hl7.org/fhir/administrative-gender"}})
 
-(t/deftest ^:kaocha/pending stu3-load-all-test
+(def blacklist
+  {"StructureDefinition"
+   #{"http://hl7.org/fhir/StructureDefinition/allergyintolerance-substanceExposureRisk"
+     "http://hl7.org/fhir/StructureDefinition/cqif-measureInfo"
+     "http://hl7.org/fhir/StructureDefinition/cqif-questionnaire"
+     "http://hl7.org/fhir/StructureDefinition/diagnosticreport-genetics"
+     "http://hl7.org/fhir/StructureDefinition/elementdefinition-de"
+     "http://hl7.org/fhir/StructureDefinition/familymemberhistory-genetic"
+     "http://hl7.org/fhir/StructureDefinition/observation-genetics"
+     "http://hl7.org/fhir/StructureDefinition/patient-clinicalTrial"
+     "http://hl7.org/fhir/StructureDefinition/procedurerequest-genetics"
+     "http://hl7.org/fhir/StructureDefinition/procedurerequest-geneticsItem"}})
 
-  (fhir/load-all ztx "hl7.fhir.r3.core" {:node-modules-folder "r3/node_modules"})
+(t/deftest ^:kaocha/pending stu3-load-all-test
+  (fhir/load-all ztx nil {:node-modules-folder "r3/node_modules"
+                          :blacklist blacklist})
+
+  (gen/generate-zen-schemas ztx)
+
+  (:fhir/inter @ztx)
+
+  (gen/spit-zen-npm-modules ztx "tmp" "2.0.0")
+
 
   )
