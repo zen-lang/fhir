467984bde10c980c79de53bd71284637dad340d6
Vasiliy Kupriakov <rublag-ns@yandex.ru>
2021-10-21 15:41:03 +0300

    Fix jq script
    Co-authored-by: @ddegr <de@health-samurai.io>

--- r3/generate.sh
+++ r3/generate.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-jq -cMs '{
-           "index-version": 1,
-           "files": [
-             .[]
-             | with_entries(select(.key == ("url", "resourceType", "id", "version", "type")))
-               + {"filename": input_filename}
-             | select(.url != null)
-           ]
-         }' *.json > .index.json
+jq -cM 'with_entries(select(.key == ("url", "resourceType", "id", "version", "type")))
+        + {"filename": input_filename}
+        | select(.url != null)' *.json | jq -s '{"index_version": 1, "files": .}' > .index.json
