6e95b12bc6e2122ee3a7c57d60f92b9771ce33d9
KGOH <kgofhedgehogs@gmail.com>
2021-11-02 18:21:00 +0200

    Add nictiz load comment
    Co-authored-by: @ghrpp <profunctorlense@gmail.com>

--- src/zen/fhir/tooling.clj
+++ src/zen/fhir/tooling.clj
@@ -17,3 +17,28 @@
     (generate-zen-schemas ztx)
     (spit-zen-npm-modules ztx zrc-dir package-ver package-name)
     (prn :done)))
+
+
+(comment
+  (let [ztx (zen-core/new-context {})]
+    (load-all ztx nil {#_#_:params {"hl7.fhir.r4.core"     {:zen.fhir/package-ns 'fhir-r4}
+                                    "hl7.fhir.r3.core"     {:zen.fhir/package-ns 'fhir-r3}
+                                    "hl7.fhir.us.core"     {:zen.fhir/package-ns 'us-core-v3}
+                                    "hl7.fhir.us.mcode"    {:zen.fhir/package-ns 'mcode-v1}
+                                    "hl7.fhir.us.carin-bb" {:zen.fhir/package-ns 'carin-bb-v1}}
+                       :node-modules-folder "/home/puck/Work/sansara/box/libs/fhir/r3/node_modules"
+                       :blacklist  {"StructureDefinition"
+                                    #{"http://hl7.org/fhir/StructureDefinition/allergyintolerance-substanceExposureRisk"
+                                      "http://hl7.org/fhir/StructureDefinition/cqif-measureInfo"
+                                      "http://hl7.org/fhir/StructureDefinition/cqif-questionnaire"
+                                      "http://hl7.org/fhir/StructureDefinition/diagnosticreport-genetics"
+                                      "http://hl7.org/fhir/StructureDefinition/elementdefinition-de"
+                                      "http://hl7.org/fhir/StructureDefinition/familymemberhistory-genetic"
+                                      "http://hl7.org/fhir/StructureDefinition/observation-genetics"
+                                      "http://hl7.org/fhir/StructureDefinition/patient-clinicalTrial"
+                                      "http://hl7.org/fhir/StructureDefinition/procedurerequest-genetics"
+                                      "http://hl7.org/fhir/StructureDefinition/procedurerequest-geneticsItem"}}})
+    (generate-zen-schemas ztx)
+    (spit-zen-npm-modules ztx "/tmp/r3-zen/node_modules" "test")
+    (prn :done))
+  nil)
