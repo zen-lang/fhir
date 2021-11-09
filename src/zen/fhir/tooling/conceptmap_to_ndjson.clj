2f672c463d3c2b30d34a885d82eaf66a391caf5a
ApricotLace <profunctorlense@gmail.com>
2021-11-09 19:04:34 +0300

    Implement ConceptMap -> .ndjson.gz bundle

---
+++ src/zen/fhir/tooling/conceptmap_to_ndjson.clj
@@ -0,0 +1,28 @@
+(ns zen.fhir.tooling.conceptmap-to-ndjson
+  (:gen-class)
+  (:require [cheshire.core :as json]
+            [clojure.java.io :as io]))
+
+(defn write-line [w x]
+  (.write w (cheshire.core/generate-string x))
+  (.write w "\n"))
+
+(defn conceptmap-to-ndjson-gz [path-to-conceptmap output-path]
+  (let [parsed-conceptmap (json/parse-string (slurp path-to-conceptmap) keyword)
+        conceptmap' (dissoc parsed-conceptmap :group)
+        maprules (mapcat (fn [group] ;;NOTE: Ungroup group by element
+                           (reduce (fn [acc element]
+                                     (conj acc (-> group (assoc :element element))))
+                                   [] (:element group)))
+                         (:group parsed-conceptmap))]
+    (with-open [gzip (-> output-path
+                         (io/output-stream)
+                         (java.util.zip.GZIPOutputStream. true)
+                         (java.io.OutputStreamWriter.)
+                         (java.io.BufferedWriter.))]
+      (write-line gzip conceptmap')
+      (doseq [m maprules]
+        (write-line gzip m)))))
+
+(defn -main [path-to-conceptmap output-path]
+  (conceptmap-to-ndjson-gz path-to-conceptmap output-path))
