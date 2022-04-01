2138c6ebf2aa48d4539e892f3f9f61acd1bbf2fb
niquola <niquola@gmail.com>
2022-04-01 18:11:38 +0300

    fix cli

--- src/zen/fhir/cli.clj
+++ src/zen/fhir/cli.clj
@@ -7,31 +7,45 @@
             [edamame.core]
             [clojure.string :as str]))
 
-
 (defn parse-edn [file]
-  (println "FILE:" file)
   (try (-> file slurp edamame.core/parse-string)
        (catch Exception e (println e))))
 
 
+(defn read-files-recursive [ztx file]
+  (if (.isDirectory file)
+    (->> (.listFiles file)
+         (filter (fn [file]
+                   (or (and (.isDirectory file) (not= "node_modules" (.getName file)))
+                       (str/ends-with? (.getName file) ".edn"))))
+         (mapv #(read-files-recursive ztx %)))
+    (do 
+      (println :load file)
+      (zen-core/load-ns ztx (parse-edn file)))))
+
 (defn read-zen-project [ztx path]
   (let [file (io/file path)]
-    (if (.isDirectory file)
-      (->> (.listFiles file)
-           (filter (fn [file]
-                     (and (not= "node_modules" (.getName file))
-                          (str/ends-with? (.getName file) ".edn"))))
-           (mapv #(read-zen-project ztx (.getPath %))))
-      (zen-core/load-ns ztx (parse-edn file))))) ;;Doesn't werks :(
+    (read-files-recursive ztx file)))
 
 
 (defn lint-project [path]
-  (let [ztx (zen-core/new-context {})]
+  (let [ztx (zen-core/new-context {:paths [path]})]
     (read-zen-project ztx path)
-    ztx))
+    (zen.core/errors ztx)))
 
 
 (comment
-  (lint-project "/Users/ghrp/prog/zen-samples/tlon-core/zrc")
+  (lint-project "/Users/niquola/zen-samples/tlon-core/zrc")
+
+  (def path "/Users/niquola/zen-samples/tlon-core/zrc")
+
+  (def ztx (zen-core/new-context {:paths [path]}))
+
+  (zen.core/read-ns ztx 'tlon.core)
+  (zen.core/read-ns ztx 'tlon.core.patient)
+  (zen.core/read-ns ztx 'tlon.core.patient)
+
+  (zen.core/errors ztx)
+  
 
   )
