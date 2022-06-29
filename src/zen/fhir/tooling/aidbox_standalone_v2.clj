bcf8359a9106622472028689bbd9f2ea3eca1165
Evgeny Mukha <profunctorlense@gmail.com>
2022-06-29 19:25:05 +0300

    Add `build-standalone-profiles-all-versions` task

---
+++ src/zen/fhir/tooling/aidbox_standalone_v2.clj
@@ -0,0 +1,36 @@
+(ns zen.fhir.tooling.aidbox-standalone-v2
+  (:gen-class)
+  (:require [zen.core :as zen-core]
+            [zen.fhir.core :refer [load-all]]
+            [zen.fhir.generator :refer [spit-zen-modules
+                                        generate-zen-schemas
+                                        packages-deps-nses]]
+            [clojure.java.shell :as shell]
+            [clojure.string :as str]))
+
+
+(defn replace-dots [s]
+  (str/replace s "." "-"))
+
+
+(defn -main [node-modules-folder zrc-dir & {:as opts}]
+  (let [{:as opts, :strs [omit-deps? preserve-package alt-package-name]} opts
+        ztx (zen-core/new-context {})]
+    (load-all ztx nil {:node-modules-folder node-modules-folder})
+    (generate-zen-schemas ztx)
+    (let [packages-deps (packages-deps-nses (:fhir/inter @ztx))]
+      (doseq [[package-name deps] packages-deps
+              :when (= package-name (symbol (replace-dots preserve-package)))]
+        (let [package-name package-name
+              standalone-dir (str zrc-dir "/" package-name "/")
+              alt-package-name (or alt-package-name package-name)]
+          (if omit-deps?
+            (spit-zen-modules ztx standalone-dir package-name)
+            (doseq [package (cons package-name deps)]
+              (spit-zen-modules ztx standalone-dir package)))
+          (shell/sh "bash" "-c" (str "cd " zrc-dir "/" package-name
+                                     " && zip -r " alt-package-name ".zip ."
+                                     " && mv " alt-package-name ".zip ../"
+                                     " && cd .."
+                                     " && rm -rf " package-name)))))
+    (prn :done)))
