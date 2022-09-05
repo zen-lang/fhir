e6d86f2c7ea6967fd3347335a5db54d4df46ef9b
Evgeny Mukha <profunctorlense@gmail.com>
2022-09-05 16:55:22 +0300

    FTR POC Start
    Co-authored-by: @islambegkatibov <79331750+islambegk@users.noreply.github.com>

---
+++ src/ftr/utils/core.clj
@@ -0,0 +1,62 @@
+(ns ftr.utils.core)
+
+(defn dissoc-when-kv
+  ([pred m k]
+   (cond-> m
+     (and (contains? m k)
+          (pred [k (get m k)]))
+     (dissoc k)))
+  ([pred m k & ks]
+   (reduce (partial dissoc-when-kv pred)
+           (dissoc-when-kv pred m k)
+           ks)))
+
+
+(defn dissoc-when-key
+  ([pred m k]
+   (cond-> m
+     (and (contains? m k)
+          (pred k))
+     (dissoc k)))
+  ([pred m k & ks]
+   (reduce (partial dissoc-when-key pred)
+           (dissoc-when-key pred m k)
+           ks)))
+
+
+(defn dissoc-when
+  ([pred m k]
+   (cond-> m
+     (and (contains? m k)
+          (pred (get m k)))
+     (dissoc k)))
+  ([pred m k & ks]
+   (reduce (partial dissoc-when pred)
+           (dissoc-when pred m k)
+           ks)))
+
+
+(defn dissoc-nil [m k & ks]
+  (apply dissoc-when nil? m k ks))
+
+
+(defn strip-when-key [pred m]
+  (if-let [ks (seq (keys m))]
+    (apply dissoc-when-key pred m ks)
+    m))
+
+
+(defn strip-when-kv [pred m]
+  (if-let [ks (seq (keys m))]
+    (apply dissoc-when-kv pred m ks)
+    m))
+
+
+(defn strip-when [pred m]
+  (if-let [ks (seq (keys m))]
+    (apply dissoc-when pred m ks)
+    m))
+
+
+(defn strip-nils [m]
+  (strip-when nil? m))
