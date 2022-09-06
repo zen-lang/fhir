2738d23767aa373681dec91733398f20afac60b0
Islambeg Katibov <islambeg.katibov@health-samurai.io>
2022-09-06 17:52:23 +0300

    Add SHA-256 to generated terminology file
    Co-authored-by: @ghrpp <profunctorlense@gmail.com>

--- src/ftr/core.clj
+++ src/ftr/core.clj
@@ -17,30 +17,20 @@
   (let [{:keys [value-set code-system concepts]}
         (ftr.extraction.core/extract cfg)
         ftr-dir (create-ftr-dir-if-not-exists! (:ftr-path cfg))
+        ftr-dir-abs-path (.getAbsolutePath ftr-dir)
         sorted-concepts (sort-by #(format "%s-%s" (:system :%) (:code %)) concepts)
-        terminology-file (io/file (str (.getAbsolutePath ftr-dir) '/ "tf.ndjson"))]
-    (with-open )
-    (spit (get-tf-path "newtf.ndjson") (str (cheshire.core/generate-string (into (sorted-map) code-system)) \newline) :append true)
-    (spit (get-tf-path "newtf.ndjson") (str (cheshire.core/generate-string (into (sorted-map) value-set)) \newline) :append true)
-    (doseq [c sorted-concepts]
-      (spit (get-tf-path "newtf.ndjson") (str (cheshire.core/generate-string c) \newline) :append true))
-    (let [sha256 (ftr.utils.core/calculate-sha256 newtf)])))
+        terminology-file (io/file (str ftr-dir-abs-path '/ "tf.ndjson"))]
+    (.delete terminology-file)
+    (with-open [fw (io/writer terminology-file :append true)]
+      (.write fw (str (cheshire.core/generate-string (into (sorted-map) code-system)) \newline))
+      (.write fw (str (cheshire.core/generate-string (into (sorted-map) value-set)) \newline))
+      (doseq [c sorted-concepts]
+        (.write fw (str (cheshire.core/generate-string c) \newline))))
+    (let [sha256 (ftr.utils.core/calculate-sha256 (.getAbsolutePath terminology-file))]
+      (.renameTo terminology-file
+                 (io/file (str ftr-dir-abs-path '/ "tf." sha256 ".ndjson"))))))
 
 (comment
-
-  (def f (io/file "/tmp/e"))
-
-  (with-open (io/))
-
-  (let [file-path "/tmp/e"
-        sha256 (ftr.utils.core/calculate-sha256 file-path)]
-    (.renameTo (io/file file-path)
-               (io/file (clojure.string/join
-                         "/"
-                         (concat
-                          (drop-last (clojure.string/split file-path #"/"))
-                          (list (str "e" \. sha256 \. "ndjson")))))))
-
   (def config {:source-url "https://storage.googleapis.com/aidbox-public/documentation/icd10_example_no_header.csv"
                :ftr-path "/tmp/myftr"
                :source-type :csv
@@ -58,16 +48,5 @@
                :value-set   {:id "icd10", :url "http://hl7.org/fhir/ValueSet/icd-10"}})
 
   (spit-ftr config)
-
-  (defn calculate-sha256 [source]
-    (let [digest (java.security.MessageDigest/getInstance "SHA-256")]
-      (with-open [input-stream  (io/input-stream source)
-                  digest-stream (java.security.DigestInputStream. input-stream digest)
-                  output-stream (io/output-stream "/dev/null")]
-        (io/copy digest-stream output-stream)
-        )
-      (format "%032x" (BigInteger. 1 (.digest digest)))))
-
-  (copy+sha256 "/tmp/x.nd")
 
   )
