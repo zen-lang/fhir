1e3d4dd2e634903abade5fb66e6713cee6d5f988
KGOH <kgofhedgehogs@gmail.com>
2022-09-21 17:35:15 +0200

    Add github release workflow WIP
    Co-authored-by: @deREXte <derexte@gmail.com>

--- src/zen/fhir/tooling_packages.clj
+++ src/zen/fhir/tooling_packages.clj
@@ -3,24 +3,33 @@
   (:require [zen.core]
             [zen.fhir.loader]
             [zen.fhir.generator]
-            [zen.fhir.writer]))
+            [zen.fhir.writer]
+            [clojure.string]))
 
 
-(defn -main [{:keys [node-modules-folder
-                     out-dir
-                     package-name
-                     zen-fhir-lib-url
-                     git-url-format]
-              :or   {zen-fhir-lib-url "git@github.com:zen-fhir/zen.fhir.git"
-                     git-url-format   "git@github.com:zen-fhir/%s.git"}}]
-  (let [github-token (System/getenv "ZEN_FHIR_RELEASE_GITHUB_TOKEN")
+(defn github-release-zen-packages [{:keys [node-modules-folder out-dir package-name zen-fhir-lib-url git-url-format]}]
+  (let [zen-fhir-lib-url (or zen-fhir-lib-url "git@github.com:zen-fhir/zen.fhir.git")
+        git-url-format   (or git-url-format "git@github.com:zen-fhir/%s.git")
+        github-token (System/getenv "ZEN_FHIR_RELEASE_GITHUB_TOKEN")
         org-name     (System/getenv "ZEN_FHIR_RELEASE_GITHUB_ORG")
         ztx (zen.core/new-context {:env {:github-token github-token}
-                                   :org-name org-name})]
-    (zen.fhir.loader/load-all ztx nil {:node-modules-folder node-modules-folder})
-    (zen.fhir.generator/generate-zen-schemas ztx)
-    (zen.fhir.writer/release-packages ztx {:out-dir          out-dir
-                                           :package          package-name
-                                           :git-url-format   git-url-format
-                                           :zen-fhir-lib-url zen-fhir-lib-url})
-    (prn :done)))
+                                   :org-name org-name})
+        _ (zen.fhir.loader/load-all ztx nil {:node-modules-folder node-modules-folder})
+        _ (zen.fhir.generator/generate-zen-schemas ztx)
+        release-result (zen.fhir.writer/release-packages ztx {:out-dir          out-dir
+                                                              :package          package-name
+                                                              :git-url-format   git-url-format
+                                                              :zen-fhir-lib-url zen-fhir-lib-url})]
+    (->> release-result
+         (mapv :package-git-url)
+         (clojure.string/join " "))))
+
+
+(defn -main [return-path node-modules-folder out-dir & [zen-fhir-lib-url git-url-format package-name]]
+  (spit return-path
+        (github-release-zen-packages
+          {:node-modules-folder node-modules-folder
+           :out-dir             out-dir
+           :package-name        package-name
+           :zen-fhir-lib-url    zen-fhir-lib-url
+           :git-url-format      git-url-format})))
