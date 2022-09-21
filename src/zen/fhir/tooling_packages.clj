569c47bb392032cdba679ea67ced2e40bb41ff78
KGOH <kgofhedgehogs@gmail.com>
2022-09-21 16:52:35 +0200

    Add zen package tooling github config
    Co-authored-by: @deREXte <derexte@gmail.com>

--- src/zen/fhir/tooling_packages.clj
+++ src/zen/fhir/tooling_packages.clj
@@ -13,7 +13,10 @@
                      git-url-format]
               :or   {zen-fhir-lib-url "git@github.com:zen-fhir/zen.fhir.git"
                      git-url-format   "git@github.com:zen-fhir/%s.git"}}]
-  (let [ztx (zen.core/new-context {})]
+  (let [github-token (System/getenv "GITHUB_TOKEN")
+        org-name     (System/getenv "ZEN_FHIR_RELEASE_GITHUB_ORG")
+        ztx (zen.core/new-context {:env {:github-token github-token}
+                                   :org-name org-name})]
     (zen.fhir.loader/load-all ztx nil {:node-modules-folder node-modules-folder})
     (zen.fhir.generator/generate-zen-schemas ztx)
     (zen.fhir.writer/release-packages ztx {:out-dir          out-dir
