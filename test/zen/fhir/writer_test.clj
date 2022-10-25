9373f60f2cc695bc7009daf91763af8009f8190a
Evgeny Mukha <profunctorlense@gmail.com>
2022-10-25 13:43:22 +0200

    Rename ftr-index builder function

--- test/zen/fhir/writer_test.clj
+++ test/zen/fhir/writer_test.clj
@@ -777,7 +777,7 @@
                    'fhir-r4.Patient])))
 
   (t/testing "vs validation works"
-    (ftr.zen-package/ftr->memory-cache zctx)
+    (ftr.zen-package/build-ftr-index zctx)
 
     (matcho/match (ftr.zen-package/validate zctx #{'fhir-r4.Patient/schema} {:gender "incorrect-value"})
                   {:errors [{:type ":zen.fhir/value-set"
