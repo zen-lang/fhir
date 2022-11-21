6315ac35fd7c276f359504e3589cfe97b58d771e
KGOH <kgofhedgehogs@gmail.com>
2022-11-21 18:59:27 +0200

    Add nz-base etl! fn
    Co-authored-by: @ghrpp <profunctorlense@gmail.com>

--- src/zen/fhir/igs_etl/nz_base.clj
+++ src/zen/fhir/igs_etl/nz_base.clj
@@ -4,13 +4,13 @@
             [clojure.java.shell]))
 
 
-(defn fhir-ig->fhir-package! [ig-path package-dir-dest]
+(defn fhir-ig->fhir-package! [{:keys [ig-path package-dir-dest]}]
   (let [package-manifest (cheshire.core/parse-string
                            (slurp (str ig-path "site/package.manifest.json"))
                            keyword)
-        package-name (:name package-manifest)
-        _ (assert (some? package-name) "Must have package name")
-        package-dest (str package-dir-dest "/" package-name)]
+        package-name     (:name package-manifest)
+        _                (assert (some? package-name) "Must have package name")
+        package-dest     (str package-dir-dest "/" package-name)]
     [(clojure.java.shell/sh "mkdir" "-p" package-dest)
      (clojure.java.shell/sh "sh" "-c" (str "cp -r " ig-path "/site/*.json " package-dest))
      (clojure.java.shell/sh "rm" "canonicals.json" "expansions.json" "usage-stats.json"
@@ -19,7 +19,19 @@
                             :dir package-dest)]))
 
 
+(defn etl! [{:keys [package-dir-dest]}]
+  (let [ig-zip-url      "https://fhir.org.nz/ig/base/full-ig.zip"
+        tmp-ig-dir-dest "/tmp/zen.fhir.igs-etl.nz-base/"
+        ig-dir          "ig/"
+        ig-path         (str tmp-ig-dir-dest \/ ig-dir)]
+    (clojure.java.shell/sh "mkdir" "-p" tmp-ig-dir-dest)
+    (zen.utils/unzip! ig-zip-url ig-path)
+    (fhir-ig->fhir-package! {:ig-path ig-path :package-dir-dest package-dir-dest})))
+
+
 (comment
+  ;; clojure -X zen.fhir.igs-etl.nz-base/etl! :package-dir-dest '"/tmp/node_modules"'
+
   (def ig-zip-url "https://fhir.org.nz/ig/base/full-ig.zip")
 
   (def dest "/tmp/zen.fhir.igs-etl.nz-base/")
