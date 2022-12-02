c272607e19c1bdfacb1f465f1bfb6ff676cff63e
KGOH <kgofhedgehogs@gmail.com>
2022-12-02 16:49:29 +0200

    Unify preload terminology with ftr

--- src/zen/fhir/loader.clj
+++ src/zen/fhir/loader.clj
@@ -2,7 +2,7 @@
   (:require [zen.fhir.search-parameter.loader :as search-parameter.loader]
             [zen.fhir.structure-definition.loader :as structure-definition.loader]
             [zen.core :as zen]
-            [ftr.extraction.ig.value-set-expand]
+            [ftr.extraction.ig.core]
             [cheshire.core]
             [clojure.java.io :as io]
             [fipp.edn]
@@ -601,57 +601,13 @@
                   (assoc :zen.fhir/resource concept))))))
 
 
-(def loader-keys
-  #{:zen/loader
-    :zen.fhir/loader
-    :zen.fhir/package
-    :zen.fhir/package-ns
-    :zen.fhir/schema-ns
-    :zen.fhir/file
-    :zen.fhir/header
-    :zen.fhir/version})
+(def loader-keys ftr.extraction.ig.core/loader-keys)
 
 
-(defmethod process-on-load :ValueSet
-  [res]
-  (merge
-    res
-    (when-let [package-ns (:zen.fhir/package-ns res)]
-      {:zen.fhir/package-ns package-ns
-       :zen.fhir/schema-ns (symbol (str (name package-ns) \. "value-set" \. (:id res)))
-       :zen.fhir/resource (apply dissoc res loader-keys)
-       :fhir/concepts (let [inter-part (select-keys res loader-keys)
-                            concepts (->> (select-keys (:compose res) [:include :exclude])
-                                          vals
-                                          (apply concat)
-                                          (filter :concept))
-                            concepts (or (some->> [:expansion :contains]
-                                                  (get-in res)
-                                                  not-empty
-                                                  (map #(assoc % :valueset [(:url res)]))
-                                                  (group-by :system)
-                                                  (reduce-kv (fn [acc system concepts]
-                                                               (conj acc {:system system :concept concepts}))
-                                                             [])
-                                                  (into concepts))
-                                         concepts)]
-                        (->> concepts
-                             (mapcat (fn [{:keys [system concept]}]
-                                       (extract-concepts inter-part
-                                                         (fn [{:keys [code]}] (str/replace (str system \/ code) \/ \-))
-                                                         system
-                                                         concept)))))})))
+(defmethod process-on-load :ValueSet [res] (ftr.extraction.ig.core/process-on-load res))
 
 
-(defmethod process-on-load :CodeSystem
-  [res]
-  (merge
-   (dissoc res :concept)
-   {:fhir/concepts (extract-concepts (select-keys res loader-keys)
-                                     (fn [{:keys [code]}] (str/replace (str (:url res) \/ code) \/ \-))
-                                     (:url res)
-                                     (:concept res))}
-   {:zen.fhir/resource (apply dissoc res :concept loader-keys)}))
+(defmethod process-on-load :CodeSystem [res] (ftr.extraction.ig.core/process-on-load res))
 
 
 (defmethod process-on-load :StructureDefinition
@@ -702,38 +658,13 @@
       :else (println :skip-resource "no url or rt" (get-in res [:zen/loader :file])))))
 
 
-(defn read-json [f] (cheshire.core/parse-string (slurp f) keyword))
-
-(defn collect-concepts [ztx]
-  (let [code-systems (vals (get-in @ztx [:fhir/inter "CodeSystem"]))
-        value-sets (vals (get-in @ztx [:fhir/inter "ValueSet"]))
-        concepts (transduce (comp (mapcat :fhir/concepts)
-                                  (map (fn [concept]
-                                         {:path [(:system concept)
-                                                 (:id concept)]
-                                          :value concept})))
-                            (completing
-                              (fn [acc {:keys [path value]}]
-                                (update-in acc path merge value)))
-                            {}
-                            (concat value-sets code-systems))]
-    (swap! ztx assoc-in [:fhir/inter "Concept"] concepts)))
+(def read-json ftr.extraction.ig.core/read-json)
 
 
-(defn process-concept [_ztx concept]
-  (-> concept
-      (assoc-in [:zen.fhir/resource :valueset]
-                (vec (:valueset concept)))))
+(def collect-concepts  ftr.extraction.ig.core/collect-concepts)
+(def process-concept   ftr.extraction.ig.core/process-concept)
+(def process-concepts  ftr.extraction.ig.core/process-concepts)
 
-
-(defn process-concepts [ztx]
-  (collect-concepts ztx)
-  (ftr.extraction.ig.value-set-expand/denormalize-value-sets-into-concepts
-    ztx)
-  (swap! ztx update-in [:fhir/inter "Concept"]
-         #(sp/transform [sp/MAP-VALS]
-                        (partial process-concept ztx)
-                        (reduce merge (vals %)))))
 
 (defn process-resources
   "this is processing of resources with context"
@@ -741,7 +672,7 @@
   (structure-definition.loader/process-structure-definitions ztx)
   (search-parameter.loader/process-search-parameters ztx)
   (when-not skip-concept-processing
-    (process-concepts ztx)))
+    (ftr.extraction.ig.core/process-concepts ztx)))
 
 
 (defn dir? [^java.io.File file]
@@ -750,28 +681,13 @@
 
 
 ;; TODO write test with all corner cases of npm dir organization
-(defn find-packages [project-root]
-  (->> [(io/file project-root)
-        (io/file (str project-root "/node_modules"))]
-       (mapcat (fn [dir] (when (dir? dir) (cons dir (.listFiles dir)))))
-       (mapcat (fn [x] (if (and (dir? x) (str/starts-with? (.getName x) "@"))
-                         (.listFiles x)
-                         [x])))
-       (filter dir?)
-       distinct
-       (filter (fn [f] (.exists (io/file (str (.getPath f) "/package.json")))))))
+(def find-packages ftr.extraction.ig.core/find-packages)
 
 
-
-(def package-blacklist
-  #{"hl7.fhir.r2.examples"
-    "hl7.fhir.r2b.examples"
-    "hl7.fhir.r3.examples"
-    "hl7.fhir.r4.examples"})
+(def package-blacklist ftr.extraction.ig.core/package-blacklist)
 
 
-(defn blacklisted-package? [package]
-  (contains? package-blacklist (:name package)))
+(def blacklisted-package? ftr.extraction.ig.core/blacklisted-package?)
 
 
 (defn do-load-file [ztx {:as opts :keys [whitelist blacklist params]} package f]
@@ -824,8 +740,8 @@
 (defn preload-all [ztx & [{:keys [params node-modules-folder whitelist blacklist skip-concept-processing]
                            :or {node-modules-folder "node_modules"}}]]
   (init-ztx ztx)
-  (doseq [pkg-dir  (find-packages node-modules-folder)]
-    (let [package (read-json (str (.getPath pkg-dir) "/package.json"))
+  (doseq [pkg-dir  (ftr.extraction.ig.core/find-packages node-modules-folder)]
+    (let [package (ftr.extraction.ig.core/read-json (str (.getPath pkg-dir) "/package.json"))
           package-params (get params (:name package))]
       (assert package (str "No package for " pkg-dir))
       (doseq [f (.listFiles pkg-dir)]
