7893ad9cd8b79e351a01c70885d751f8a8bff4c7
islambeg katibov <islambeg@proton.me>
2023-02-20 21:36:04 +0300

    Support additional national New Zealand FHIR IG packages
    Supported NZ IG packages are specified in `zen.fhir.igs-etl.nz/igs`.
    
    Co-authored-by: @yurii_makhotkin <kgofhedgehogs@gmail.com>

--- src/zen/fhir/igs_etl/nz.clj
+++ src/zen/fhir/igs_etl/nz.clj
@@ -19,15 +19,21 @@
     {:manifest updated-manifest}))
 
 
-(defn fhir-ig->fhir-package! [{:keys [ig-path package-dir-dest]}]
+(defn fhir-ig-site->fhir-package!
+  "Should be used only when `package.tgz`, describing IG, is not
+  available.
+
+  `ig-path` is a path to a `site` folder, contained in the usual
+  distributions of zipped IGs."
+  [{:keys [ig-path package-dir-dest]}]
   (let [package-manifest (cheshire.core/parse-string
-                           (slurp (io/file ig-path "site/package.manifest.json"))
+                           (slurp (io/file ig-path "/package.manifest.json"))
                            keyword)
         package-name     (:name package-manifest)
         _                (assert (some? package-name) "Must have package name")
         package-dest     (.getPath (io/file package-dir-dest package-name))]
     [(clojure.java.shell/sh "mkdir" "-p" package-dest)
-     (clojure.java.shell/sh "sh" "-c" (str "cp -r " ig-path "/site/*.json " package-dest))
+     (clojure.java.shell/sh "sh" "-c" (str "cp -r " ig-path "/*.json " package-dest))
      (clojure.java.shell/sh "rm" "canonicals.json" "expansions.json" "usage-stats.json"
                             :dir package-dest)
      (clojure.java.shell/sh "mv" "package.manifest.json" "package.json"
@@ -35,17 +41,52 @@
      (add-fhir-core-dependency-to-manifest! (io/file package-dest "package.json"))]))
 
 
+(defn fhir-ig->fhir-package!
+  "`ig-path` is a path to a `package` folder, which is unpacked from
+  `site/package.tgz` contained in the usual distributions of zipped
+  IGs."
+  [{:keys [ig-path package-dir-dest]}]
+  (let [package-manifest (cheshire.core/parse-string
+                          (slurp (io/file ig-path "package.json"))
+                          keyword)
+        package-name     (:name package-manifest)
+        _                (assert (some? package-name) "Must have package name")
+        package-dest     (.getPath (io/file package-dir-dest package-name))]
+    [(clojure.java.shell/sh "mkdir" "-p" package-dest)
+     (clojure.java.shell/sh "sh" "-c" (str "cp -r " ig-path "/*.json " package-dest))
+     (clojure.java.shell/sh "rm" "canonicals.json" "expansions.json" "usage-stats.json"
+                            :dir package-dest)]))
+
+
 (def igs [{:url      "https://fhir.org.nz/ig/base/full-ig.zip"
-           :dest-dir "zen.fhir.igs-etl.nz-base"}])
+           :dir "zen.fhir.igs-etl.nz-base"}
+          {:url "https://nhi-ig.hip-uat.digital.health.nz/full-ig.zip"
+           :dir "zen.fhir.igs-etl.nz-nhi"}
+          {:url "https://hpi-ig.hip-uat.digital.health.nz/full-ig.zip"
+           :dir "zen.fhir.igs-etl.nz-hpi"}
+          {:url "http://build.fhir.org/ig/HL7NZ/mdr/full-ig.zip"
+           :dir "zen.fhir.igs-etl.nz-mdr"}])
 
 
 (defn etl! [{:keys [package-dir-dest]}]
   (let [work-dir-path "/tmp"
         ig-dir "ig"]
-    (doseq [{:keys [url dest-dir]} igs
-            :let [dest-path (.getPath (io/file work-dir-path dest-dir))]]
-      (zen.utils/unzip! url dest-path)
-      (fhir-ig->fhir-package! {:ig-path dest-path :package-dir-dest package-dir-dest}))))
+    (doseq [{:keys [url dir]} igs
+            :let [download-path (.getPath (io/file work-dir-path dir))
+                  ig-work-dir-path (.getPath (io/file download-path "site"))]]
+      (zen.utils/unzip! url download-path)
+      (let [package-archive (io/file ig-work-dir-path "package.tgz")]
+        (if (.exists package-archive)
+          (do
+            (clojure.java.shell/sh "tar" "-xzf" (.getPath package-archive) "-C" ig-work-dir-path)
+            (fhir-ig->fhir-package! {:ig-path
+                                     (.getPath (io/file ig-work-dir-path "package"))
+                                     :package-dir-dest
+                                     package-dir-dest}))
+          (fhir-ig-site->fhir-package! {:ig-path
+                                        ig-work-dir-path
+                                        :package-dir-dest
+                                        package-dir-dest}))))))
 
 
 (comment
