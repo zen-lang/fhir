93ae74f1dde8aa4d7f1b75a0fb8bf888a73e5ed8
KGOH <kgofhedgehogs@gmail.com>
2023-02-24 02:13:28 +0200

    Add fhir light converter element process WIP

--- src/zen/fhir_light/loader.clj
+++ src/zen/fhir_light/loader.clj
@@ -1,1 +1,75 @@
-(ns zen.fhir-light.loader)
+(ns zen.fhir-light.loader
+  (:require [clojure.string :as str]))
+
+
+(def elements-keys-types
+  {:loc         #{:id :path}
+   :validation  #{:min :max :type :condition :constraint}
+   :meta        #{:mustSupport}
+   :description #{:mapping}})
+
+
+(defn- group-element-keys [element]
+  (update-vals elements-keys-types #(select-keys element %)))
+
+
+(def ^:private poly-postfix "[x]")
+
+
+(defn- drop-poly-terminator [id]
+  (subs id 0 (- (count id) (count poly-postfix))))
+
+
+(defn- poly-root? [key-part]
+  (str/ends-with? key-part poly-postfix))
+
+
+(defn- parse-id-part [id-part]
+  (let [[key-part slice-part] (str/split id-part #":" 2)]
+    (cond
+      (poly-root? key-part)
+      (let [poly-root (drop-poly-terminator key-part)]
+        (if (some? slice-part)
+          [{:type :poly-key, :poly-key (keyword slice-part) :poly-root poly-root}]
+          [{:type :poly-root, :poly-root poly-root}]))
+
+      (some? slice-part)
+      [{:type :key, :key (keyword key-part)}
+       {:type :slice, :slice slice-part}]
+
+      :else
+      [{:type :key, :key (keyword key-part)}])))
+
+
+(defn- parse-id [id]
+  (if (str/blank? id)
+    []
+    (let [id-parts (str/split (str id) #"\.")]
+      (vec (cons {:type :root, :root (first id-parts)}
+                 (mapcat parse-id-part (rest id-parts)))))))
+
+
+(defn- enrich-loc [grouped-element]
+  (let [id (get-in grouped-element [:loc :id])]
+    (assoc-in grouped-element [:loc :zen.fhir-light/id] (parse-id id))))
+
+
+(defn- parsed-id->nested-path [parsed-id]
+  (mapcat
+    (fn [id-el]
+      (case (:type id-el)
+        :root      []
+        :key       [:els (:key id-el)]
+        :poly-root [:poly-roots (:poly-root id-el)]
+        :poly-key  [:poly-keys (:poly-key id-el)]
+        :slice     [:slicing :slices (:slice id-el)]))
+    parsed-id))
+
+
+(defn- nest-by-enriched-path [enriched-elements]
+  (:result (reduce (fn [acc el]
+                     (assoc-in acc
+                               (cons :result (parsed-id->nested-path (get-in el [:loc :zen.fhir-light/id])))
+                               el))
+                   {:result {}}
+                   enriched-elements)))
--- test/zen/fhir_light/loader_test.clj
+++ test/zen/fhir_light/loader_test.clj
@@ -1,1 +1,70 @@
-(ns zen.fhir-light.loader-test)
+(ns zen.fhir-light.loader-test
+  (:require [cheshire.core :as json]
+            [clojure.test :as t]
+            [matcho.core :as matcho]
+            [zen.core]
+            [zen.fhir-light.loader :as sut]))
+
+
+(t/deftest convert-single-strdef-test
+  (def dir (System/getProperty "user.dir"))
+
+  (def fhir-core-ig-dir
+    (str dir "/node_modules/hl7.fhir.r4.core"))
+
+  (def us-core-ig-dir
+    (str dir "/node_modules/hl7.fhir.us.core"))
+
+  (def us-core-patient-str-def
+    (json/parse-string (slurp (str us-core-ig-dir "/StructureDefinition-us-core-patient.json"))
+                       keyword))
+  #_(dissoc us-core-patient-str-def :text :snapshot)
+
+  (def ztx (zen.core/new-context {}))
+
+  (t/testing "pure convertation, no context"
+    (def el-res (map #'sut/group-element-keys (get-in us-core-patient-str-def [:differential :element])))
+
+    (matcho/match
+      el-res
+      [{:loc {:id "Patient"                          :path "Patient"}                   :validation {:constraint [{} nil]}}
+       {:loc {:id "Patient.extension:race"           :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
+       {:loc {:id "Patient.extension:ethnicity"      :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
+       {:loc {:id "Patient.extension:birthsex"       :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
+       {:loc {:id "Patient.extension:genderIdentity" :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
+       {:loc {:id "Patient.identifier"               :path "Patient.identifier"}        :validation {:min 1} :meta {:mustSupport true} :description {:mapping [{}]}}
+       {:loc {:id "Patient.identifier.system"        :path "Patient.identifier.system"} :validation {:min 1}}
+       {:loc {:id "Patient.identifier.value"         :path "Patient.identifier.value"}  :validation {:min 1}}
+       {:loc {:id "Patient.name"                     :path "Patient.name"}              :validation {:min 1}}
+       {:loc {:id "Patient.name.use"                 :path "Patient.name.use"}          :validation empty?}
+       {:loc {:id "Patient.name.family"              :path "Patient.name.family"}       :validation {:condition ["us-core-6"]}}
+       {:loc {:id "Patient.name.given"               :path "Patient.name.given"}        :validation {:condition ["us-core-6"]}}
+       {:loc {:id "Patient.name.suffix"              :path "Patient.name.suffix"}       :validation empty?}])
+
+    (def enriched-res (map #'sut/enrich-loc el-res))
+
+    (matcho/match
+      enriched-res
+      [{:loc {:id "Patient"                   :zen.fhir-light/id [{:root "Patient" :type :root}]}}
+       {:loc {:id "Patient.extension:race"    :zen.fhir-light/id [{:root "Patient"}
+                                                                  {:key :extension :type :key}
+                                                                  {:slice "race"   :type :slice}]}}
+       {} {} {} {}
+       {:loc {:id "Patient.identifier.system" :zen.fhir-light/id [{:root "Patient"}
+                                                                  {:key :identifier :type :key}
+                                                                  {:key :system     :type :key}]}}])
+
+    (def nested-res (#'sut/nest-by-enriched-path (mapv #(dissoc % :description :meta) enriched-res)))
+
+    (matcho/match
+      nested-res
+      {:loc {:id "Patient"}
+       :els {:extension {:slicing {:slices {"race" {:loc {:id "Patient.extension:race"}}}}}
+             :identifier {:els {:system {:loc {:id "Patient.identifier.system"}}}}}})))
+
+(t/deftest convert-many-strdef-test
+  (t/is true))
+
+
+(t/deftest convert-ig-package-test
+  (t/is true))
