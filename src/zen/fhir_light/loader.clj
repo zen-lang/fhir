e31b6e51aff0cdde19bc12577c002715a960dd6c
KGOH <kgofhedgehogs@gmail.com>
2023-02-24 17:04:45 +0200

    Add schema parts building

--- src/zen/fhir_light/loader.clj
+++ src/zen/fhir_light/loader.clj
@@ -3,7 +3,6 @@
             [zen.fhir.utils :as utils]))
 
 
-
 (def elements-keys-types
   {:loc         #{:id :path}
    :validation  #{:base :binding :condition :constraint :contentReference
@@ -78,28 +77,154 @@
 
 
 (defn- enrich-loc [grouped-element]
-  (let [id          (get-in grouped-element [:loc :id])
-        parsed-id   (parse-id id)
-        nested-path (parsed-id->nested-path parsed-id)]
-    (update grouped-element :loc
-            assoc
-            ::id parsed-id
-            ::path nested-path)))
+  (let [id        (get-in grouped-element [:loc :id])
+        parsed-id (parse-id id)]
+    (assoc-in grouped-element [:loc ::id] parsed-id)))
 
 
-(def ^:private default-keys-to-strip
-  #{:loc :description :meta})
+#{:base
+  :binding
+  :condition
+  :constraint
+  :contentReference
+  :sliceIsConstraining
+  :sliceName
+  :slicing
+  :type}
 
 
-(defn- nest-by-enriched-loc [enriched-elements
-                             & {:keys [keys-to-strip]
-                                :or {keys-to-strip default-keys-to-strip}}]
+(def validation-keys-types
+  {:value     #{:type :binding :contentReference :maxLength #_[:base :path]}
+   :container #{:max :min :sliceIsConstraining :sliceName :slicing #_[:base :min] #_[:base :max]}
+   :outer     #{:max :min :condition #_[:base :min] #_[:base :max]}
+   :context   #{:constraint}})
+
+
+(defmulti el-keys->zen
+  (fn [keys-type [el-key _el-val]]
+    [keys-type el-key]))
+
+
+(defmethod el-keys->zen :default [_ _] nil)
+
+
+(defn validation->zen-schema-parts [validation]
+  (not-empty
+    (into {}
+          (keep (fn [[keys-type el-keys]]
+                  (when-let [schema-part
+                             (not-empty (into {}
+                                              (map #(el-keys->zen keys-type %))
+                                              (select-keys validation el-keys)))]
+                    [keys-type schema-part])))
+          validation-keys-types)))
+
+
+(defn- add-schema-parts [grouped-el]
+  (utils/assoc-some
+    grouped-el
+    :schema-parts
+    (validation->zen-schema-parts (:validation grouped-el))))
+
+
+(defmethod el-keys->zen [:outer :min] [_ [_ min-card]]
+  (when (< 0 min-card)
+    {::required true}))
+
+
+(defmethod el-keys->zen [:container :min] [_ [_ min-card]]
+  (when (< 0 min-card)
+    {::min min-card}))
+
+
+(defmethod el-keys->zen [:outer :max] [_ [_ max-card]]
+  (when (= 0 max-card)
+    {::forbidden true}))
+
+
+(defmethod el-keys->zen [:container :max] [_ [_ max-card]]
+  (if (= "*" max-card)
+    {::collection true}
+    {::max (parse-long max-card)}))
+
+
+(defmethod el-keys->zen [:value :maxLength] [_ [_ max-length]]
+  {:type 'zen/string
+   :maxLength max-length})
+
+
+(defn- parsed-id->nested-path [parsed-id]
+  (vec (mapcat (fn [id-el]
+                 (case (:type id-el)
+                   :root      []
+                   :key       [:els (:key id-el)]
+                   :poly-root [:poly-roots (:poly-root id-el)]
+                   :poly-key  [:poly-keys (:poly-key id-el)]
+                   :slice     [:slicing :slices (:slice id-el)]))
+         parsed-id)))
+
+
+(defmulti el-part&path
+  (fn [_parsed-id [keys-group _el-part]]
+    keys-group))
+
+
+(defmethod el-part&path :default [parsed-id [keys-group el-part]]
+  [{:el-part el-part
+    :path    (conj (parsed-id->nested-path parsed-id) keys-group)}])
+
+
+(defmulti schema-part-path
+  (fn [_parsed-id [schema-keys-type _schema-part]]
+    schema-keys-type))
+
+
+(defmethod el-part&path :schema-parts [parsed-id [_ schema-parts]]
+  (map (fn [e] {:el-part (val e)
+                :path (schema-part-path parsed-id e)})
+       schema-parts))
+
+
+(defmethod schema-part-path :value [parsed-id [_ schema-part]]
+  (conj (parsed-id->nested-path parsed-id) :value))
+
+
+(defmethod schema-part-path :container [parsed-id [_ schema-part]]
+  (conj (parsed-id->nested-path parsed-id) :container (last parsed-id)))
+
+
+(defmethod schema-part-path :outer [parsed-id [_ schema-part]]
+  (let [outer-id (->> parsed-id
+                        reverse
+                        rest
+                        (drop-while #(not= :key (:type %)))
+                        reverse)
+        outer-path (parsed-id->nested-path outer-id)]
+    (conj outer-path :els-constraints (last parsed-id))))
+
+
+(defmethod schema-part-path :context [parsed-id [_ schema-part]]
+  [:context parsed-id])
+
+
+(defn strip-el [el & {:keys [keys-to-select keys-to-strip]}]
+  (not-empty
+    (cond-> el
+      (seq keys-to-strip)
+      (as-> $ (apply dissoc $ keys-to-strip))
+
+      (seq keys-to-select)
+      (select-keys keys-to-select))))
+
+
+(defn- nest-by-enriched-loc [enriched-elements & {:as params}]
   (:result
    (transduce
-     (keep #(when-let [stripped-el (not-empty (apply dissoc % keys-to-strip))]
-              [(get-in % [:loc ::path])
-               stripped-el]))
-     (completing (fn [acc [path el]]
-                   (assoc-in acc (cons :result path) el)))
+     (mapcat (fn [el]
+               (when-let [stripped-el (strip-el el params)]
+                 (mapcat #(el-part&path (get-in el [:loc ::id]) %)
+                         stripped-el))))
+     (completing (fn [acc {:keys [el-part path]}]
+                   (assoc-in acc (cons :result path) el-part)))
      {:result {}}
      enriched-elements)))
--- test/zen/fhir_light/loader_test.clj
+++ test/zen/fhir_light/loader_test.clj
@@ -23,45 +23,52 @@
   (def ztx (zen.core/new-context {}))
 
   (t/testing "pure convertation, no context"
-    (def el-res (map #'sut/group-element-keys (get-in us-core-patient-str-def [:differential :element])))
+    (t/testing "keys grouping"
+      (def el-res (map #'sut/group-element-keys (get-in us-core-patient-str-def [:differential :element])))
 
-    (matcho/match
-      el-res
-      [{:loc {:id "Patient"                          :path "Patient"}                   :validation {:constraint [{} nil]}}
-       {:loc {:id "Patient.extension:race"           :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
-       {:loc {:id "Patient.extension:ethnicity"      :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
-       {:loc {:id "Patient.extension:birthsex"       :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
-       {:loc {:id "Patient.extension:genderIdentity" :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
-       {:loc {:id "Patient.identifier"               :path "Patient.identifier"}        :validation {:min 1} :meta {:mustSupport true} :description {:mapping [{}]}}
-       {:loc {:id "Patient.identifier.system"        :path "Patient.identifier.system"} :validation {:min 1}}
-       {:loc {:id "Patient.identifier.value"         :path "Patient.identifier.value"}  :validation {:min 1}}
-       {:loc {:id "Patient.name"                     :path "Patient.name"}              :validation {:min 1}}
-       {:loc {:id "Patient.name.use"                 :path "Patient.name.use"}          :validation empty?}
-       {:loc {:id "Patient.name.family"              :path "Patient.name.family"}       :validation {:condition ["us-core-6"]}}
-       {:loc {:id "Patient.name.given"               :path "Patient.name.given"}        :validation {:condition ["us-core-6"]}}
-       {:loc {:id "Patient.name.suffix"              :path "Patient.name.suffix"}       :validation empty?}])
+      (matcho/match
+        el-res
+        [{:loc {:id "Patient"                          :path "Patient"}                   :validation {:constraint [{} nil]}}
+         {:loc {:id "Patient.extension:race"           :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
+         {:loc {:id "Patient.extension:ethnicity"      :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
+         {:loc {:id "Patient.extension:birthsex"       :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
+         {:loc {:id "Patient.extension:genderIdentity" :path "Patient.extension"}         :validation {:min 0 :max "1" :type [{:code "Extension" :profile [string?]}]}}
+         {:loc {:id "Patient.identifier"               :path "Patient.identifier"}        :validation {:min 1} :meta {:mustSupport true} :description {:mapping [{}]}}
+         {:loc {:id "Patient.identifier.system"        :path "Patient.identifier.system"} :validation {:min 1}}
+         {:loc {:id "Patient.identifier.value"         :path "Patient.identifier.value"}  :validation {:min 1}}
+         {:loc {:id "Patient.name"                     :path "Patient.name"}              :validation {:min 1}}
+         {:loc {:id "Patient.name.use"                 :path "Patient.name.use"}          :validation empty?}
+         {:loc {:id "Patient.name.family"              :path "Patient.name.family"}       :validation {:condition ["us-core-6"]}}
+         {:loc {:id "Patient.name.given"               :path "Patient.name.given"}        :validation {:condition ["us-core-6"]}}
+         {:loc {:id "Patient.name.suffix"              :path "Patient.name.suffix"}       :validation empty?}]))
 
-    (def enriched-res (map #'sut/enrich-loc el-res))
+    (t/testing "parse id"
+      (def enriched-res (map #'sut/enrich-loc el-res))
 
-    (matcho/match
-      enriched-res
-      [{:loc {:id "Patient"                   ::sut/id [{:root "Patient" :type :root}]}}
-       {:loc {:id "Patient.extension:race"    ::sut/id [{:root "Patient"}
-                                                        {:key :extension :type :key}
-                                                        {:slice "race"   :type :slice}]}}
-       {} {} {} {}
-       {:loc {:id "Patient.identifier.system" ::sut/id [{:root "Patient"}
-                                                        {:key :identifier :type :key}
-                                                        {:key :system     :type :key}]}}])
+      (matcho/match
+        enriched-res
+        [{:loc {:id "Patient"                   ::sut/id [{:root "Patient" :type :root}]}}
+         {:loc {:id "Patient.extension:race"    ::sut/id [{:root "Patient"}
+                                                          {:key :extension :type :key}
+                                                          {:slice "race"   :type :slice}]}}
+         {} {} {} {}
+         {:loc {:id "Patient.identifier.system" ::sut/id [{:root "Patient"}
+                                                          {:key :identifier :type :key}
+                                                          {:key :system     :type :key}]}}]))
 
-    (def nested-res (#'sut/nest-by-enriched-loc enriched-res))
+    (t/testing "generate schema parts"
+      (def schema-parts-res (map #'sut/add-schema-parts enriched-res)))
 
-    (matcho/match
-      nested-res
-      {:validation {:constraint [{}]}
-       :els {:extension {:slicing {:slices {"race" {:validation {:type [{:profile []}]}}}}}
-             :identifier {:els {:system {:validation {:min 1}}}}
-             :telecom {:els {:system {:validation {:binding {}}}}}}})))
+    (t/testing "nesting"
+      (def nested-res (#'sut/nest-by-enriched-loc
+                        schema-parts-res
+                        :keys-to-strip #{:loc :description :meta :validation}))
+
+      (matcho/match
+        nested-res
+        {:els {:extension {:slicing {:slices {"race" {}}}}
+               :identifier {:els {:system {}}}
+               :telecom {:els {:system {}}}}}))))
 
 
 (t/deftest convert-many-strdef-test
